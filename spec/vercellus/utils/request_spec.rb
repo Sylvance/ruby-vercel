10367ccd03ac38e9987b0c05fdf95a71a3a9ce46
Sylvance Mbaka <kerandisylvance@gmail.com>
2021-05-28 09:01:06 +0300

    Fix lint issues for real

--- spec/vercellus/utils/request_spec.rb
+++ spec/vercellus/utils/request_spec.rb
@@ -1,44 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe Vercellus::Utils::Request do
-  let(:response) { double(:body => '{}') }
-  let(:url) { 'deployments' }
-  let(:headers) do
-    {
-      'Content-Type': 'application/json',
-      'Authorization': "Bearer #{Vercellus.configuration.token}"
-    }
-  end
-  let(:params) { {limit: 10} }
+  let(:response) { double(body: "{}") }
+  let(:headers) { { "Content-Type": "application/json", "Authorization": "Bearer #{Vercellus.configuration.token}" } }
+  let(:params) { { limit: 10 } }
 
-  context '#get' do
-    it 'does a http get request with Faraday gem' do
-      expect(Faraday).to receive(:get)
-        .with("https://api.vercel.com/#{url}", headers: headers, params: params)
-        .and_return(response)
-
+  context "#get" do
+    it "does a http get request with Faraday gem" do
+      expect(Faraday).to receive(:get).with("https://api.vercel.com/url", headers: headers,
+                                                                          params: params).and_return(response)
       Vercellus::Utils::Request.get(url, nil, params)
     end
   end
 
-  context '#post' do
-    let(:body) { { name: 'my-instant-deployment' } }
+  context "#post" do
+    let(:body) { { name: "my-instant-deployment" } }
 
-    it 'does a http post request with Faraday gem' do
-      expect(Faraday).to receive(:post)
-        .with("https://api.vercel.com/#{url}", headers: headers, params: params, body: body.to_json)
-        .and_return(response)
-
+    it "does a http post request with Faraday gem" do
+      expect(Faraday).to receive(:post).with("https://api.vercel.com/url", headers: headers, params: params,
+                                                                           body: body.to_json).and_return(response)
       Vercellus::Utils::Request.post(url, params, body)
     end
   end
 
-  context '#delete' do
-    it 'does a http delete request with Faraday gem' do
-      expect(Faraday).to receive(:delete)
-        .with("https://api.vercel.com/#{url}", headers: headers, params: params)
-        .and_return(response)
-
+  context "#delete" do
+    it "does a http delete request with Faraday gem" do
+      expect(Faraday).to receive(:delete).with("https://api.vercel.com/url", headers: headers,
+                                                                             params: params).and_return(response)
       Vercellus::Utils::Request.delete(url, nil, params)
     end
   end
